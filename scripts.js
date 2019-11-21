3ceaeab9e3ab50a3fae6ecfaf6fc48d7ca30f309
Kivanc <kivancdundar@outlook.com>
2019-11-21 14:16:25 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,9 @@ dragElement(document.getElementById("image1"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  if (document.getElementById(elmnt.id)) {
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
