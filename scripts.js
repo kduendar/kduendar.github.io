0bc2f6d4c4cbd8f3194555e530b6d9aba6a0abc4
Kivanc <kivancdundar@outlook.com>
2021-01-31 14:59:00 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,3 +85,41 @@ function randomNr2(){
    var x = Math.floor((Math.random() * 710) + 90); //random number between 90 and 800
    return x;
 }
+
+
+
+window.onload = function() {
+  
+  var a = document.getElementById("order");
+  
+  a.onclick = function() {
+    document.getElementById('th_1').style.top = 65 + 'px';
+    document.getElementById('th_1').style.left = 210 + 'px';
+    document.getElementById('th_2').style.top = 65 + 'px';
+    document.getElementById('th_2').style.left = 430 + 'px';
+    document.getElementById('th_3').style.top = 65 + 'px';
+    document.getElementById('th_3').style.left = 650 + 'px';
+    document.getElementById('th_4').style.top = 65 + 'px';
+    document.getElementById('th_4').style.left = 870 + 'px';
+    document.getElementById('th_5').style.top = 219 + 'px';
+    document.getElementById('th_5').style.left = 210 + 'px';
+    document.getElementById('th_6').style.top = 219 + 'px';
+    document.getElementById('th_6').style.left = 430 + 'px';
+    document.getElementById('th_7').style.top = 219 + 'px';
+    document.getElementById('th_7').style.left = 650 + 'px';
+    document.getElementById('th_8').style.top = 219 + 'px';
+    document.getElementById('th_8').style.left = 870 + 'px';
+    document.getElementById('th_9').style.top = 373 + 'px';
+    document.getElementById('th_9').style.left = 210 + 'px';
+    document.getElementById('th_10').style.top = 373 + 'px';
+    document.getElementById('th_10').style.left = 430 + 'px';
+    document.getElementById('th_11').style.top = 373 + 'px';
+    document.getElementById('th_11').style.left = 650 + 'px';
+    document.getElementById('th_12').style.top = 373 + 'px';
+    document.getElementById('th_12').style.left = 870 + 'px';
+    
+    return false;
+  }
+}
+
+
