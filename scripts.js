083bb3ecf79dcb0dd792744b1225bfe68ef5951a
Kivanc <kivancdundar@outlook.com>
2021-01-31 16:12:11 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,114 +8,135 @@ for (i = 0; i < thumbnails.length; i++) {
 shuffleThumbnails();
 
 function shuffleThumbnails() {
-    var j;
-    for (j = 0; j < thumbnails.length; j++) {
-        thumbnails[j].style.top = randomNr1() + 'px';
-        thumbnails[j].style.left = randomNr2() + 'px';
-    }
-    return false;
+	document.getElementById('th_1').style.top = randomNr1() + 'px';
+	document.getElementById('th_1').style.left = randomNr2() + 'px';
+	document.getElementById('th_2').style.top = randomNr1() + 'px';
+	document.getElementById('th_2').style.left = randomNr2() + 'px';
+	document.getElementById('th_3').style.top = randomNr1() + 'px';
+	document.getElementById('th_3').style.left = randomNr2() + 'px';
+	document.getElementById('th_4').style.top = randomNr1() + 'px';
+	document.getElementById('th_4').style.left = randomNr2() + 'px';
+	document.getElementById('th_5').style.top = randomNr1() + 'px';
+	document.getElementById('th_5').style.left = randomNr2() + 'px';
+	document.getElementById('th_6').style.top = randomNr1() + 'px';
+	document.getElementById('th_6').style.left = randomNr2() + 'px';
+	document.getElementById('th_7').style.top = randomNr1() + 'px';
+	document.getElementById('th_7').style.left = randomNr2() + 'px';
+	document.getElementById('th_8').style.top = randomNr1() + 'px';
+	document.getElementById('th_8').style.left = randomNr2() + 'px';
+	document.getElementById('th_9').style.top = randomNr1() + 'px';
+	document.getElementById('th_9').style.left = randomNr2() + 'px';
+	document.getElementById('th_10').style.top = randomNr1() + 'px';
+	document.getElementById('th_10').style.left = randomNr2() + 'px';
+	document.getElementById('th_11').style.top = randomNr1() + 'px';
+	document.getElementById('th_11').style.left = randomNr2() + 'px';
+	document.getElementById('th_12').style.top = randomNr1() + 'px';
+	document.getElementById('th_12').style.left = randomNr2() + 'px';
+	return false;
 }
- 
+
 function orderThumbnails() {
-	thumbnails[0].style.top = 65 + 'px';
-    thumbnails[0]style.left = 210 + 'px';
-    thumbnails[1]style.top = 65 + 'px';
-    thumbnails[1]style.left = 430 + 'px';
-    thumbnails[2]style.top = 65 + 'px';
-    thumbnails[2]style.left = 650 + 'px';
-    thumbnails[3]style.top = 65 + 'px';
-    thumbnails[3]style.left = 870 + 'px';
-    thumbnails[4]style.top = 219 + 'px';
-    thumbnails[4]style.left = 210 + 'px';
-    thumbnails[5]style.top = 219 + 'px';
-    thumbnails[5]style.left = 430 + 'px';
-    thumbnails[6]style.top = 219 + 'px';
-    thumbnails[6]style.left = 650 + 'px';
-    thumbnails[7]style.top = 219 + 'px';
-    thumbnails[7]style.left = 870 + 'px';
-    thumbnails[8]style.top = 373 + 'px';
-    thumbnails[8]style.left = 210 + 'px';
-    thumbnails[9].style.top = 373 + 'px';
-    thumbnails[9].style.left = 430 + 'px';
-    thumbnails[10].style.top = 373 + 'px';
-    thumbnails[10].style.left = 650 + 'px';
-    thumbnails[11].style.top = 373 + 'px';
-    thumbnails[11].style.left = 870 + 'px';
-    return false;
+	document.getElementById('th_1').style.top = 65 + 'px';
+	document.getElementById('th_1').style.left = 210 + 'px';
+	document.getElementById('th_2').style.top = 65 + 'px';
+	document.getElementById('th_2').style.left = 430 + 'px';
+	document.getElementById('th_3').style.top = 65 + 'px';
+	document.getElementById('th_3').style.left = 650 + 'px';
+	document.getElementById('th_4').style.top = 65 + 'px';
+	document.getElementById('th_4').style.left = 870 + 'px';
+	document.getElementById('th_5').style.top = 219 + 'px';
+	document.getElementById('th_5').style.left = 210 + 'px';
+	document.getElementById('th_6').style.top = 219 + 'px';
+	document.getElementById('th_6').style.left = 430 + 'px';
+	document.getElementById('th_7').style.top = 219 + 'px';
+	document.getElementById('th_7').style.left = 650 + 'px';
+	document.getElementById('th_8').style.top = 219 + 'px';
+	document.getElementById('th_8').style.left = 870 + 'px';
+	document.getElementById('th_9').style.top = 373 + 'px';
+	document.getElementById('th_9').style.left = 210 + 'px';
+	document.getElementById('th_10').style.top = 373 + 'px';
+	document.getElementById('th_10').style.left = 430 + 'px';
+	document.getElementById('th_11').style.top = 373 + 'px';
+	document.getElementById('th_11').style.left = 650 + 'px';
+	document.getElementById('th_12').style.top = 373 + 'px';
+	document.getElementById('th_12').style.left = 870 + 'px';
+	return false;
 }
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    
-  if (document.getElementById(elmnt.id)) {
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-  } else {
-    elmnt.onmousedown = dragMouseDown;
-  }
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+	if (document.getElementById(elmnt.id)) {
+		document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+	} else {
+		elmnt.onmousedown = dragMouseDown;
+	}
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		// stop moving when mouse button is released:
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
 }
 
 function randomNr1(){
-   var x = Math.floor((Math.random() * 600) + 0); //random number between 0 and 600
-   return x;
+	var x = Math.floor((Math.random() * 600) + 0); //random number between 0 and 600
+	return x;
 }
 
 function randomNr2(){
-   var x = Math.floor((Math.random() * 710) + 90); //random number between 90 and 800
-   return x;
+	var x = Math.floor((Math.random() * 710) + 90); //random number between 90 and 800
+	return x;
 }
 
 window.onload = function() {
-  var orderButton = document.getElementById("order");
-  orderButton.onclick = function() {
-    orderThumbnails()
-    return false;
-  }
-  
-  var thumbnailClass = document.getElementsByClassName("thumbnail");
-  var colorizeButton = document.getElementById("colorize"); 
-  var resetButton = document.getElementById("reset"); 
-  colorizeButton.onclick = function() {
-    thumbnailClass.setAttribute("style", "-webkit-filter:grayscale(0%)");
-    thumbnailClass.setAttribute("style", "filter:grayscale(0%)");
-    //thumbnails.style.filter: "grayscale(0%)";
-    return false;
-  }
-  resetButton.onclick = function() {
-    thumbnailClass.setAttribute("style", "-webkit-filter:grayscale(100%)");
-    thumbnailClass.setAttribute("style", "filter:grayscale(100%)");
-    //thumbnails.style.filter: "grayscale(0%)";
-    return false;
-  }
+	var orderButton = document.getElementById("order");
+	
+	orderButton.onclick = function() {
+		orderThumbnails()
+		return false;
+	}
+
+	var thumbnailClass = document.getElementsByClassName("thumbnail");
+	var colorizeButton = document.getElementById("colorize"); 
+	var resetButton = document.getElementById("reset"); 
+	
+	colorizeButton.onclick = function() {
+		thumbnailClass.setAttribute("style", "-webkit-filter:grayscale(0%)");
+		thumbnailClass.setAttribute("style", "filter:grayscale(0%)");
+		//thumbnails.style.filter: "grayscale(0%)";
+		return false;
+	}
+	resetButton.onclick = function() {
+		thumbnailClass.setAttribute("style", "-webkit-filter:grayscale(100%)");
+		thumbnailClass.setAttribute("style", "filter:grayscale(100%)");
+		//thumbnails.style.filter: "grayscale(0%)";
+		return false;
+	}
 }
 
 
